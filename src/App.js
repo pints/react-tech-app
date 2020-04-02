db609883074b33c63d60d86a63d1cb048d16ea8a
Angadi <PAngadi@ad.harman.com>
2020-04-02 11:53:39 +0530

    project folder

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <h1>Sample tech app</h1>
+  );
+}
+
+export default App;
